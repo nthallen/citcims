b6128447932922bca4d0717965ed513843a7f20c
Norton Allen <allen@huarp.harvard.edu>
2019-05-24 08:04:52 -0600

    ToF Horiba: Added 7th Flow Controller

--- ToF/TM/Horiba.cmd
+++ ToF/TM/Horiba.cmd
@@ -22,4 +22,6 @@
     { $0.channel = 5; $0.value = $3; }
   : Zero Flow %f (Enter value in sccm) sccm
     { $0.channel = 6; $0.value = $3; }
+  : Cal2 Flow %f (Enter value in sccm) sccm
+    { $0.channel = 7; $0.value = $3; }
 ;
