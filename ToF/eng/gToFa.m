fba00ea179f6d48d26d7dc406726d8a209660663
Norton Allen <allen@huarp.harvard.edu>
2019-05-28 18:52:19 -0600

    Cleanup eng directory

--- ToF/eng/gToFa.m
+++
@@ -1,5 +0,0 @@
-function fig = gToFa(varargin);
-% gToFa(...)
-% Attitude
-ffig = ne_group(varargin,'Attitude','pToFaa','pToFap','pToFar','pToFass','pToFad','pToFasz','pToFaaz','pToFa_az','pToFat');
-if nargout > 0 fig = ffig; end
--- ToF/eng/gToFw.m
+++
@@ -1,5 +0,0 @@
-function fig = gToFw(varargin);
-% gToFw(...)
-% Wind
-ffig = ne_group(varargin,'Wind','pToFwvv','pToFwvs','pToFwd','pToFws');
-if nargout > 0 fig = ffig; end
