19d3c20738e902a4e62c85fea0de41a94f932d42
Norton Allen <allen@huarp.harvard.edu>
2019-09-10 13:35:25 -0400

    Allocate additional space for signed outputs

--- ToF/eng/data_field.m
+++ ToF/eng/data_field.m
@@ -1,54 +1,60 @@
 classdef data_field < handle
-    % data_field represents two text objects,
-    % a label and the value display, and the corresponding
-    % data stream
-    properties(GetAccess = public)
-        flds % The parent data_fields object
-        rec_name
-        var_name
-        fmt
-        lbl_name
-        lbl
-        txt
-        lbl_width
-        txt_width
-        fld_height
+  % data_field represents two text objects,
+  % a label and the value display, and the corresponding
+  % data stream
+  properties(GetAccess = public)
+    flds % The parent data_fields object
+    rec_name
+    var_name
+    fmt
+    lbl_name
+    lbl
+    txt
+    lbl_width
+    txt_width
+    fld_height
+  end
+  methods
+    function df = data_field(flds, rec_name, var_name, fmt, signed)
+      if nargin < 5
+        signed = false;
+      end
+      df.flds = flds;
+      df.fmt = fmt;
+      df.rec_name = rec_name;
+      df.var_name = var_name;
+      df.lbl_name = [rec_name '_lbl'];
+      lbltxt = [var_name ':'];
+      df.lbl = uicontrol(flds.fig, ...
+        'Style', 'text', 'String', lbltxt, ...
+        'HorizontalAlignment', 'left', ...
+        'BackgroundColor', flds.figbgcolor, ...
+        'FontWeight', 'bold', ...
+        'tag', df.lbl_name, ...
+        'uicontextmenu', flds.dfuicontextmenu, ...
+        'userdata', df);
+      df.lbl_width = df.lbl.Extent(3);
+      str = df.txt_convert(0);
+      if signed
+        str = [ '-' str ];
+      end
+      df.txt = uicontrol(flds.fig, ...
+        'Style', 'text', 'String', str, ...
+        'HorizontalAlignment', 'right', ...
+        'BackgroundColor', [1 1 1], ...
+        'FontName', flds.opts.txt_font, ...
+        'FontSize', flds.opts.txt_fontsize, ...
+        'tag', df.var_name);
+      df.txt_width = df.txt.Extent(3);
+      df.fld_height = max(df.lbl.Position(4), df.txt.Position(4));
     end
-    methods
-        function df = data_field(flds, rec_name, var_name, fmt)
-            df.flds = flds;
-            df.fmt = fmt;
-            df.rec_name = rec_name;
-            df.var_name = var_name;
-            df.lbl_name = [rec_name '_lbl'];
-            lbltxt = [var_name ':'];
-            df.lbl = uicontrol(flds.fig, ...
-                'Style', 'text', 'String', lbltxt, ...
-                'HorizontalAlignment', 'left', ...
-                'BackgroundColor', flds.figbgcolor, ...
-                'FontWeight', 'bold', ...
-                'tag', df.lbl_name, ...
-                'uicontextmenu', flds.dfuicontextmenu, ...
-                'userdata', df);
-            df.lbl_width = df.lbl.Extent(3);
-            str = ['-' df.txt_convert(0)];
-            df.txt = uicontrol(flds.fig, ...
-                'Style', 'text', 'String', str, ...
-                'HorizontalAlignment', 'right', ...
-                'BackgroundColor', [1 1 1], ...
-                'FontName', flds.opts.txt_font, ...
-                'FontSize', flds.opts.txt_fontsize, ...
-                'tag', df.var_name);
-            df.txt_width = df.txt.Extent(3);
-            df.fld_height = max(df.lbl.Position(4), df.txt.Position(4));
-        end
-        
-        function str = txt_convert(obj, val)
-            if isnumeric(obj.fmt)
-                str = num2str(val,obj.fmt);
-            else
-                str = sprintf(obj.fmt,val);
-            end
-        end
+    
+    function str = txt_convert(obj, val)
+      if isnumeric(obj.fmt)
+        str = num2str(val,obj.fmt);
+      else
+        str = sprintf(obj.fmt,val);
+      end
     end
+  end
 end
--- ToF/eng/data_fields.m
+++ ToF/eng/data_fields.m
@@ -1,14 +1,14 @@
 classdef data_fields < handle
-    % data_fields currently encompasses all the data fields in a project,
-    % all assumed to be on the same page. This will need to be augmented
-    % to support multiple panes or figures. Possibly a higher-level
-    % class to encompass multiple pages.
-    %
-    % This class will keep track of the position of fields on the figure.
-    properties
-        fig
-        figbgcolor
-        opts
+  % data_fields currently encompasses all the data fields in a project,
+  % all assumed to be on the same page. This will need to be augmented
+  % to support multiple panes or figures. Possibly a higher-level
+  % class to encompass multiple pages.
+  %
+  % This class will keep track of the position of fields on the figure.
+  properties
+    fig
+    figbgcolor
+    opts
     %       min_y
     %       max_y
     %       min_x
@@ -19,196 +19,199 @@ classdef data_fields < handle
     %       h_leading % space between label and text and unit
     %       col_leading % horizontal space between columns
     %       txt_padding
-        cur_x
-        cur_y
-        records % data_records object
-        fields
-        % fields will be indexed like records, i.e.
-        % obj.fields.(rec).vars.(var) will be an array of data_field
-        % objects, allowing a variable to appear in more than one
-        % location
-        cur_col
-        % cur_col will record fields in the current column
-        % We need to keep this until the column is closed, so we can
-        % adjust the column widths.
-        % cur_col.fields will be a cell array of data_field objects
-        % cur_col.n_rows will be a scalar count of elements in fields
-        % cur_col.max_lbl_width will be the current maximum label width
-        % cur_col.max_txt_width will be the current maximum text width
-        graph_figs % cell array of data_fig objects
-        dfuicontextmenu % uicontextmenu for data_field lables
-        gimenu % The 'Graph in:' submenu
+    cur_x
+    cur_y
+    records % data_records object
+    fields
+    % fields will be indexed like records, i.e.
+    % obj.fields.(rec).vars.(var) will be an array of data_field
+    % objects, allowing a variable to appear in more than one
+    % location
+    cur_col
+    % cur_col will record fields in the current column
+    % We need to keep this until the column is closed, so we can
+    % adjust the column widths.
+    % cur_col.fields will be a cell array of data_field objects
+    % cur_col.n_rows will be a scalar count of elements in fields
+    % cur_col.max_lbl_width will be the current maximum label width
+    % cur_col.max_txt_width will be the current maximum text width
+    graph_figs % cell array of data_fig objects
+    dfuicontextmenu % uicontextmenu for data_field lables
+    gimenu % The 'Graph in:' submenu
+  end
+  methods
+    function obj = data_fields(fig_in, varargin)
+      obj.fig = fig_in;
+      % verify that fig is an object
+      % record dimensions
+      set(obj.fig,'units','pixels');
+      d = get(obj.fig,'Position');
+      obj.opts.min_x = 0;
+      obj.opts.max_x = d(3);
+      obj.opts.min_y = 0;
+      obj.opts.max_y = d(4);
+      obj.opts.v_padding = 10;
+      obj.opts.v_leading = 3;
+      obj.opts.h_padding = 20;
+      obj.opts.h_leading = 0;
+      obj.opts.col_leading = 15;
+      obj.opts.txt_padding = 5;
+      obj.opts.txt_font = 'Courier New';
+      obj.opts.txt_fontsize = 10;
+      for i = 1:2:length(varargin)
+        fld = varargin{i};
+        if isfield(obj.opts, fld)
+          obj.opts.(fld) = varargin{i+1};
+        else
+          error('MATLAB:LE:badopt', 'Invalid option: "%s"', fld);
+        end
+      end
+      obj.cur_x = obj.opts.min_x + obj.opts.h_padding;
+      obj.cur_y = obj.opts.max_y;
+      obj.records = data_records();
+      obj.figbgcolor = get(obj.fig,'Color');
+      obj.graph_figs = {};
+      obj.dfuicontextmenu = uicontextmenu(fig_in);
+      obj.gimenu = uimenu(obj.dfuicontextmenu,'Label','Graph in:');
+      uimenu(obj.gimenu,'Label','New figure', ...
+        'Callback', { @data_fields.context_callback, "new_fig"});
     end
-    methods
-        function obj = data_fields(fig_in, varargin)
-            obj.fig = fig_in;
-            % verify that fig is an object
-            % record dimensions
-            set(obj.fig,'units','pixels');
-            d = get(obj.fig,'Position');
-            obj.opts.min_x = 0;
-            obj.opts.max_x = d(3);
-            obj.opts.min_y = 0;
-            obj.opts.max_y = d(4);
-            obj.opts.v_padding = 10;
-            obj.opts.v_leading = 3;
-            obj.opts.h_padding = 20;
-            obj.opts.h_leading = 0;
-            obj.opts.col_leading = 15;
-            obj.opts.txt_padding = 5;
-            obj.opts.txt_font = 'Courier New';
-            obj.opts.txt_fontsize = 10;
-            for i = 1:2:length(varargin)
-                fld = varargin{i};
-                if isfield(obj.opts, fld)
-                    obj.opts.(fld) = varargin{i+1};
-                else
-                    error('MATLAB:LE:badopt', 'Invalid option: "%s"', fld);
-                end
-            end
-            obj.cur_x = obj.opts.min_x + obj.opts.h_padding;
-            obj.cur_y = obj.opts.max_y;
-            obj.records = data_records();
-            obj.figbgcolor = get(obj.fig,'Color');
-            obj.graph_figs = {};
-            obj.dfuicontextmenu = uicontextmenu(fig_in);
-            obj.gimenu = uimenu(obj.dfuicontextmenu,'Label','Graph in:');
-            uimenu(obj.gimenu,'Label','New figure', ...
-              'Callback', { @data_fields.context_callback, "new_fig"});
-        end
-        
-        function start_col(obj)
-            obj.cur_col.fields = {};
-            obj.cur_col.n_rows = 0;
-            obj.cur_col.max_lbl_width = 0;
-            obj.cur_col.max_txt_width = 0;
-            obj.cur_y = obj.opts.max_y - obj.opts.v_padding;
-            % obj.cur_x = obj.cur_x + obj.opts.col_leading;
-        end
-        
-        function end_col(obj)
-            % reposition txt fields according to max widths
-            % txt fields are all right-justified
-            %  cur_x is the left of the lbl field
-            %  cur_x + max_lbl_width is the right edge of the lbl col
-            %  cur_x + max_lbl_width + h_padding is left edge of txt col
-            r_edge = obj.cur_x + obj.cur_col.max_lbl_width + ...
-                obj.opts.h_leading + obj.cur_col.max_txt_width;
-            for i=1:length(obj.cur_col.fields)
-                fld = obj.cur_col.fields{i};
-                pos = fld.txt.Position;
-                pos(1) = r_edge - pos(3);
-                fld.txt.Position = pos;
-            end
-            % update cur_x to the right edge of righthand fields
-            % plus col_leading, so left of new column
-            obj.cur_x = r_edge + obj.opts.col_leading;
-        end
-        
-        function df = field(dfs, rec_name, var_name, fmt)
-            dfs.records.add_record(rec_name);
-            if ~isfield(dfs.fields, rec_name) || ...
-                    ~isfield(dfs.fields.(rec_name).vars,var_name)
-                dfs.fields.(rec_name).vars.(var_name) = {};
-            end
-            df_int = data_field(dfs, rec_name, var_name, fmt);
-            dfs.fields.(rec_name).vars.(var_name){end+1} = df_int;
-            if dfs.cur_y - df_int.fld_height < dfs.opts.min_y + dfs.opts.v_padding
-                dfs.end_col();
-                dfs.start_col();
-                % we assume one row will always fit
-            end
-            dfs.cur_col.fields{end+1} = df_int;
-            dfs.cur_col.n_rows = dfs.cur_col.n_rows+1;
-            if df_int.lbl_width > dfs.cur_col.max_lbl_width
-                dfs.cur_col.max_lbl_width = df_int.lbl_width;
-            end
-            df_int.txt_width = df_int.txt_width + dfs.opts.txt_padding;
-            if df_int.txt_width > dfs.cur_col.max_txt_width
-                dfs.cur_col.max_txt_width = df_int.txt_width;
-            end
-            dfs.cur_y = dfs.cur_y - df_int.fld_height;
-            df_int.lbl.Position = ...
-                [ dfs.cur_x, dfs.cur_y, df_int.lbl_width, df_int.fld_height];
-            df_int.txt.Position = ...
-                [ dfs.cur_x + df_int.lbl_width + dfs.opts.h_leading, dfs.cur_y, ...
-                  df_int.txt_width, ...
-                  df_int.fld_height];
-            dfs.cur_y = dfs.cur_y - dfs.opts.v_leading;
-            if nargout > 0; df = df_int; end
-        end
-        function process_record(obj,rec_name,str)
-            if nargin >= 3
-                obj.records.process_record(rec_name,str);
-            end
-            % Now go through fields and update text
-            if isfield(obj.fields,rec_name)
-                flds = obj.fields.(rec_name);
-                vars = fieldnames(flds.vars);
-                for i=1:length(vars)
-                    if isfield(str,vars{i})
-                        fs = flds.vars.(vars{i});
-                        for j = 1:length(fs)
-                            set(fs{j}.txt,'String', ...
-                                fs{j}.txt_convert(str.(vars{i})));
-                        end
-                    end
-                end
-            end
-            % Now go through graph_figs
-            if nargin >= 3
-                for i=1:length(obj.graph_figs)
-                    obj.graph_figs{i}.update(rec_name);
-                end
-            end
-        end
-        function dfig = new_graph_fig(dfs)
-            dfig = data_fig(dfs, length(dfs.graph_figs)+1);
-            dfs.graph_figs{end+1} = dfig;
-        end
-        function new_graph(dfs, rec_name, var_name, mode, fignum, axisnum)
-          if mode == "new_fig"
-            dfig = dfs.new_graph_fig();
-            axisnum = 0;
-          else
-            dfig = dfs.graph_figs{fignum};
-            if mode ~= "cur_axes"
-              axisnum = 0;
+    
+    function start_col(obj)
+      obj.cur_col.fields = {};
+      obj.cur_col.n_rows = 0;
+      obj.cur_col.max_lbl_width = 0;
+      obj.cur_col.max_txt_width = 0;
+      obj.cur_y = obj.opts.max_y - obj.opts.v_padding;
+      % obj.cur_x = obj.cur_x + obj.opts.col_leading;
+    end
+    
+    function end_col(obj)
+      % reposition txt fields according to max widths
+      % txt fields are all right-justified
+      %  cur_x is the left of the lbl field
+      %  cur_x + max_lbl_width is the right edge of the lbl col
+      %  cur_x + max_lbl_width + h_padding is left edge of txt col
+      r_edge = obj.cur_x + obj.cur_col.max_lbl_width + ...
+        obj.opts.h_leading + obj.cur_col.max_txt_width;
+      for i=1:length(obj.cur_col.fields)
+        fld = obj.cur_col.fields{i};
+        pos = fld.txt.Position;
+        pos(1) = r_edge - pos(3);
+        fld.txt.Position = pos;
+      end
+      % update cur_x to the right edge of righthand fields
+      % plus col_leading, so left of new column
+      obj.cur_x = r_edge + obj.opts.col_leading;
+    end
+    
+    function df = field(dfs, rec_name, var_name, fmt, signed)
+      if nargin < 5
+        signed = false;
+      end
+      dfs.records.add_record(rec_name);
+      if ~isfield(dfs.fields, rec_name) || ...
+          ~isfield(dfs.fields.(rec_name).vars,var_name)
+        dfs.fields.(rec_name).vars.(var_name) = {};
+      end
+      df_int = data_field(dfs, rec_name, var_name, fmt, signed);
+      dfs.fields.(rec_name).vars.(var_name){end+1} = df_int;
+      if dfs.cur_y - df_int.fld_height < dfs.opts.min_y + dfs.opts.v_padding
+        dfs.end_col();
+        dfs.start_col();
+        % we assume one row will always fit
+      end
+      dfs.cur_col.fields{end+1} = df_int;
+      dfs.cur_col.n_rows = dfs.cur_col.n_rows+1;
+      if df_int.lbl_width > dfs.cur_col.max_lbl_width
+        dfs.cur_col.max_lbl_width = df_int.lbl_width;
+      end
+      df_int.txt_width = df_int.txt_width + dfs.opts.txt_padding;
+      if df_int.txt_width > dfs.cur_col.max_txt_width
+        dfs.cur_col.max_txt_width = df_int.txt_width;
+      end
+      dfs.cur_y = dfs.cur_y - df_int.fld_height;
+      df_int.lbl.Position = ...
+        [ dfs.cur_x, dfs.cur_y, df_int.lbl_width, df_int.fld_height];
+      df_int.txt.Position = ...
+        [ dfs.cur_x + df_int.lbl_width + dfs.opts.h_leading, dfs.cur_y, ...
+        df_int.txt_width, ...
+        df_int.fld_height];
+      dfs.cur_y = dfs.cur_y - dfs.opts.v_leading;
+      if nargout > 0; df = df_int; end
+    end
+    function process_record(obj,rec_name,str)
+      if nargin >= 3
+        obj.records.process_record(rec_name,str);
+      end
+      % Now go through fields and update text
+      if isfield(obj.fields,rec_name)
+        flds = obj.fields.(rec_name);
+        vars = fieldnames(flds.vars);
+        for i=1:length(vars)
+          if isfield(str,vars{i})
+            fs = flds.vars.(vars{i});
+            for j = 1:length(fs)
+              set(fs{j}.txt,'String', ...
+                fs{j}.txt_convert(str.(vars{i})));
             end
           end
-          dfig.new_graph(rec_name, var_name, mode, axisnum);
         end
-        function m = add_menu(dfs, title)
-          m = uimenu(dfs.fig,'Text',title);
-          set(dfs.fig,'Interruptible','on');
+      end
+      % Now go through graph_figs
+      if nargin >= 3
+        for i=1:length(obj.graph_figs)
+          obj.graph_figs{i}.update(rec_name);
         end
-        function add_userdata(dfs, datum)
-          set(dfs.fig,'userdata',datum);
-        end
-        function datum = get_userdata(dfs)
-          datum = get(dfs.fig,'userdata');
-        end
-    end
-    methods(Static)
-      function context_callback(~,~, mode, fignum, axisnum)
-        % lbl is the variable's label. The data_field object
-        %   should be the label's userdata
-        % mode is one of:
-        %   'new_fig' - create graph in new figure
-        %   'new_axes' - create graph in new axis in figure # fn
-        %   'cur_axes' - create graph in existing axis # an in figure # fn
-        if mode == "new_fig"
-          fignum = 0;
-          axisnum = 0;
-        elseif mode == "new_axes"
-          axisnum = 0;
-        end
-        lbl = gco;
-        df = get(lbl,'userdata');
-        rec_name = df.rec_name;
-        var_name = df.var_name;
-        dfs = df.flds;
-        dfs.new_graph(rec_name, var_name, mode, fignum, axisnum);
       end
     end
+    function dfig = new_graph_fig(dfs)
+      dfig = data_fig(dfs, length(dfs.graph_figs)+1);
+      dfs.graph_figs{end+1} = dfig;
+    end
+    function new_graph(dfs, rec_name, var_name, mode, fignum, axisnum)
+      if mode == "new_fig"
+        dfig = dfs.new_graph_fig();
+        axisnum = 0;
+      else
+        dfig = dfs.graph_figs{fignum};
+        if mode ~= "cur_axes"
+          axisnum = 0;
+        end
+      end
+      dfig.new_graph(rec_name, var_name, mode, axisnum);
+    end
+    function m = add_menu(dfs, title)
+      m = uimenu(dfs.fig,'Text',title);
+      set(dfs.fig,'Interruptible','on');
+    end
+    function add_userdata(dfs, datum)
+      set(dfs.fig,'userdata',datum);
+    end
+    function datum = get_userdata(dfs)
+      datum = get(dfs.fig,'userdata');
+    end
+  end
+  methods(Static)
+    function context_callback(~,~, mode, fignum, axisnum)
+      % lbl is the variable's label. The data_field object
+      %   should be the label's userdata
+      % mode is one of:
+      %   'new_fig' - create graph in new figure
+      %   'new_axes' - create graph in new axis in figure # fn
+      %   'cur_axes' - create graph in existing axis # an in figure # fn
+      if mode == "new_fig"
+        fignum = 0;
+        axisnum = 0;
+      elseif mode == "new_axes"
+        axisnum = 0;
+      end
+      lbl = gco;
+      df = get(lbl,'userdata');
+      rec_name = df.rec_name;
+      var_name = df.var_name;
+      dfs = df.flds;
+      dfs.new_graph(rec_name, var_name, mode, fignum, axisnum);
+    end
+  end
 end
